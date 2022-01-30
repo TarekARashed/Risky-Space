3f3bceb4ad51a3e80f51c9b80a4a029899f66a0c
TarekARashed <98634840+TarekARashed@users.noreply.github.com>
2022-01-29 20:27:54 -0700

    Update TFIDF-CAL.py

--- TFIDF-CAL.py
+++ TFIDF-CAL.py
@@ -6,6 +6,7 @@ import pandas as pd
 import glob
 from nltk.corpus import stopwords
 import os
+import pickle
 
 new_stopwords = ["all", "due", "to", "on", "daily", ',', ')', '(',':', '&','%', '?','#', '/', '.', '|', 'and', '\n', 'also']
 stpwrd = list(stopwords.words('english'))
@@ -13,16 +14,18 @@ stpwrd.extend(new_stopwords)
 Current_Directory = os.getcwd()
 TFIDF_Features_Path=os.path.join(Current_Directory, "TFIDF_Features", "")
 Text_Features_Path=os.path.join(Current_Directory, "Text_Features", "")
+TFIDF_Model_Path=os.path.join(Current_Directory, "TFIDF_Features", "TF_IDF_feature.pkl")
 if (os.path.isdir(TFIDF_Features_Path) and os.path.isdir(Text_Features_Path)):
     text_files = glob.glob(f"{Text_Features_Path}/*.txt")
     text_titles = [Path(text).stem for text in text_files]
     regex1 = '[a-zA-Z]{3,12}'
     if len(text_files)>2: 
         #Tfidf_vectorizer = TfidfVectorizer(input='filename', lowercase=True, analyzer = 'word', decode_error='ignore', stop_words=stpwrd,max_features=400, token_pattern=r'\b[^_\d\W]+\b', min_df=20)
-        Tfidf_vectorizer = TfidfVectorizer(input='filename', lowercase=True, analyzer = 'word', decode_error='ignore', stop_words=stpwrd,max_features=600, token_pattern=regex1, min_df=5)
+        Tfidf_vectorizer = TfidfVectorizer(input='filename', lowercase=True, analyzer = 'word', decode_error='ignore', stop_words=stpwrd,max_features=100, token_pattern=regex1, min_df=5)
         tf_Idf_vector = Tfidf_vectorizer.fit_transform(text_files)
         Tf_Idf= pd.DataFrame(tf_Idf_vector.toarray(), index=text_titles, columns=Tfidf_vectorizer.get_feature_names_out())
         File_Name=Path(TFIDF_Features_Path, 'TF_IDF').with_suffix('.csv')
         Tf_Idf.to_csv(File_Name, encoding = 'cp1252', errors='ignore')
+        pickle.dump(Tfidf_vectorizer.vocabulary_,open(TFIDF_Model_Path,"wb"))
     else:
         print ("There are no txt files to calcaluate TF-IDF weights")
