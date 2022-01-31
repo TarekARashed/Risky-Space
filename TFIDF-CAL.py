f55c1eb5493d5170da2d629af05d653dfc0cf4d4
TarekARashed <98634840+TarekARashed@users.noreply.github.com>
2022-01-30 18:18:33 -0700

    Update TFIDF-CAL.py

--- TFIDF-CAL.py
+++ TFIDF-CAL.py
@@ -19,9 +19,8 @@ if (os.path.isdir(TFIDF_Features_Path) and os.path.isdir(Text_Features_Path)):
     text_files = glob.glob(f"{Text_Features_Path}/*.txt")
     text_titles = [Path(text).stem for text in text_files]
     regex1 = '[a-zA-Z]{3,12}'
-    if len(text_files)>2: 
-        #Tfidf_vectorizer = TfidfVectorizer(input='filename', lowercase=True, analyzer = 'word', decode_error='ignore', stop_words=stpwrd,max_features=400, token_pattern=r'\b[^_\d\W]+\b', min_df=20)
-        Tfidf_vectorizer = TfidfVectorizer(input='filename', lowercase=True, analyzer = 'word', decode_error='ignore', stop_words=stpwrd,max_features=100, token_pattern=regex1, min_df=5)
+    if len(text_files)>2:
+        Tfidf_vectorizer = TfidfVectorizer(input='filename', lowercase=True, analyzer = 'word', decode_error='ignore', stop_words=stpwrd,max_features=550, token_pattern=regex1, min_df=5)
         tf_Idf_vector = Tfidf_vectorizer.fit_transform(text_files)
         Tf_Idf= pd.DataFrame(tf_Idf_vector.toarray(), index=text_titles, columns=Tfidf_vectorizer.get_feature_names_out())
         File_Name=Path(TFIDF_Features_Path, 'TF_IDF').with_suffix('.csv')
